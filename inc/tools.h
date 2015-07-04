6378beb026160cecb14bda372950b27facf8dba6
Abhishek Kondaveeti <abhishek.kondaveeti@gmail.com>
2015-07-04 12:24:59 +0530

    abhi-tools: chnaging copyright year

--- inc/tools.h
+++ inc/tools.h
@@ -1,12 +1,12 @@
 /*
  * tools.h
  *
- * Some basic tools for c 
+ * Some basic tools for c
  *
- * Copyright (C) 2012 Abhishek Reddy kondaveeti
+ * Copyright (C) 2015 Abhishek Reddy kondaveeti
  *
  * Contributor: Abhishek Reddy Kondaveeti <abhishek.kondaveeti@gmail.com>
- *              
+ *
  *
  * This file is licensed under the terms of the GNU General Public License
  * version 2. This program is licensed "as is" without any warranty of any
